3dffaac01c9ad0b9516c9881311170262ea2a5ee
zhouc@decent.cn <zhouchi2001>
2025-03-29 21:24:45 +0800

    集成Netty-Websokcet-Cluster

--- websocket-service/src/main/java/com/zhouzhou/cloud/websocketservice/config/ZookeeperConfig.java
+++ websocket-service/src/main/java/com/zhouzhou/cloud/websocketservice/config/ZookeeperConfig.java
@@ -4,24 +4,23 @@ import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@RefreshScope
 @Configuration
 public class ZookeeperConfig {
 
-    // 从配置文件读取 ZooKeeper 连接地址
     @Value("${zookeeper.connection-string}")
     private String connectionString;
 
     @Bean
     public CuratorFramework curatorFramework() {
-        // 创建 Curator 客户端，设置重试策略
         CuratorFramework client = CuratorFrameworkFactory.builder()
                 .connectString(connectionString)
                 .retryPolicy(new ExponentialBackoffRetry(1000, 3))
                 .build();
-        // 启动客户端
         client.start();
         return client;
     }
