dccb11e5bdcca233ddfa0502a4bd7657dbc0cfc1
zhouc@decent.cn <zhouchi2001>
2025-11-04 13:04:03 +0800

    集成skywalking链路追踪 并集成docker容器化部署

--- auth-service/src/main/java/com/zhouzhou/cloud/AuthServiceApplication.java
+++ auth-service/src/main/java/com/zhouzhou/cloud/AuthServiceApplication.java
@@ -2,6 +2,7 @@ package com.zhouzhou.cloud;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
