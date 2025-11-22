ab8d5b4cfb3dde80de72e21836706f69d2b31ac1
zhouc@decent.cn <zhouchi2001>
2025-11-22 17:39:06 +0800

    修复websocket连接相关问题

--- websocket-service/src/main/java/com/zhouzhou/cloud/websocketservice/handler/PingHeartBeatHandler.java
+++ websocket-service/src/main/java/com/zhouzhou/cloud/websocketservice/handler/PingHeartBeatHandler.java
@@ -3,6 +3,7 @@ package com.zhouzhou.cloud.websocketservice.handler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.websocketx.PingWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import io.netty.handler.timeout.IdleState;
 import io.netty.handler.timeout.IdleStateEvent;
 import lombok.extern.slf4j.Slf4j;
@@ -12,10 +13,11 @@ public class PingHeartBeatHandler extends SimpleChannelInboundHandler<PingWebSoc
 
     @Override
     public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
-        if (evt instanceof IdleStateEvent) {
-            IdleStateEvent event = (IdleStateEvent) evt;
-            if (event.state() == IdleState.WRITER_IDLE) {
-                ctx.writeAndFlush(new PingWebSocketFrame());
+        if (evt instanceof IdleStateEvent event) {
+            if (event.state() == IdleState.READER_IDLE) {
+                // 此处代码在正式的生产环境中 需要使用ping数据帧 前端需要根据ping来响应pong来完成完整完整的心跳检测机制
+//                ctx.writeAndFlush(new PingWebSocketFrame());
+                ctx.writeAndFlush(new TextWebSocketFrame("ping"));
             }
         }
         super.userEventTriggered(ctx, evt);
